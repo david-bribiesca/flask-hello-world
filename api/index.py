fc968ddb224c3ef4a34f9cb8120eba8b508b0b22
David Bribiesca Valtierra <147475727+david-bribiesca@users.noreply.github.com>
2025-11-06 07:42:44 -0600

    Implement sensor route with database connection
    Added sensor route to fetch current time from database.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+import os
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+def get_connection():
+    load_dotenv()
+    CONNECTION_STRING = os.getenv("CONN_STRING")
+    return psycopg2.connect(CONNECTION_STRING)
 
 @app.route('/')
 def home():
@@ -9,3 +17,26 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/sensor')
+def sensor():
+    try:
+        connection = get_connection()
+        print("Connection successful!")
+        
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Example query
+        cursor.execute("SELECT NOW();")
+        result = cursor.fetchone()
+        print("Current Time:", result)
+    
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+        return f"Current Time: {result}"
+    
+    except Exception as e:
+        return f"Failed to connect: {e}"
